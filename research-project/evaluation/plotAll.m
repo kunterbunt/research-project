c89d1b334ca310a9ded26f1cb2a73922969dcb68
kunterbunt <to-kunterbunt@gmx.net>
2017-03-30 21:40:26 +0200

    automated plotting in matlab with scripts

---
+++ research-project/evaluation/plotAll.m
@@ -0,0 +1,9 @@
+plotFile('../results/PositionCircle-Cellular/', ['Rx1'; 'Rx2'; 'Rx3']);
+plotFile('../results/PositionCircle-D2D-MaxDatarate/', ['Rx1'; 'Rx2'; 'Rx3']);
+plotFile('../results/PositionCircle-D2D-ProportionalFair/', ['Rx1'; 'Rx2'; 'Rx3']);
+plotFile('../results/PositionCircle-D2D-RoundRobin/', ['Rx1'; 'Rx2'; 'Rx3']);
+
+plotFile('../results/PositionOptimal-Cellular/', ['Rx1'; 'Rx2']);
+plotFile('../results/PositionOptimal-D2D-MaxDatarate/', ['Rx1'; 'Rx2']);
+plotFile('../results/PositionOptimal-D2D-ProportionalFair/', ['Rx1'; 'Rx2']);
+plotFile('../results/PositionOptimal-D2D-RoundRobin/', ['Rx1'; 'Rx2']);
--- research-project/evaluation/plotFile.m
+++ research-project/evaluation/plotFile.m
@@ -1,4 +1,14 @@
-function plotFile(filename, titles)
-values = getValuesFromFile(filename);
-plotValues(values, titles);
+function plotFile(path, labels)
+    filename = [path 'parsed'];
+    values = getValuesFromFile(filename);
+%     figure;
+%     hold on;
+    plot = bar(values);
+    axis([0, size(labels, 1) + 1,0,3200000])
+    set(gca, 'XTick', 1:3, 'XTickLabel', labels);    
+%     hold off;
+    parts = strsplit(path, '/');
+    dir = parts{end-1};
+    saveas(plot, [path dir '.png'],'png');
 end
+
