63b4b594a2838bcbd8aef577871d677bbdcfdc6b
kunterbunt <to-kunterbunt@gmx.net>
2017-05-04 13:44:51 +0200

    ParameterStudy-Bands-{DRR,MD}

--- research-project/evaluation/RBs_generatePlotForFolder.m
+++ research-project/evaluation/RBs_generatePlotForFolder.m
@@ -1,4 +1,4 @@
-function RBs_generatePlotForFolder(path, plotTitle)
+function RBs_generatePlotForFolder(path, plotTitle, ylimit)
 sums = [];
 fairnesses = [];
 for i=1:10
@@ -8,30 +8,32 @@ for i=1:10
    fairnesses(end + 1) = fairness;   
 end
 
-fig = figure('visible', 'off');
-%figure
+%fig = figure('visible', 'off');
+figure
 hold on
 title(plotTitle);
 [hAx, hLine1, hLine2] = plotyy(1:10, sums, 1:10, fairnesses);
 ylabel(hAx(1),'#bytes received') % left y-axis 
 ylabel(hAx(2),'Jains Fairness Index') % right y-axis
-set(hAx(1),'YLim',[0 8000000])
+if exist('ylimit', 'var')    
+    set(gca,'ylim', ylimit)
+end
 set(hAx(2),'YLim',[0 1.1])
 set(gca,'fontsize', 21)
 set(hAx(2),'fontsize', 21)
 xlabel('#bands');
 
-ylabh = get(hAx(2),'ylabel');
-set(ylabh,'Position', get(ylabh,'Position') + [-2 0 0])
+%ylabh = get(hAx(2),'ylabel');
+%set(ylabh,'Position', get(ylabh,'Position') + [-2 0 0])
 
-xlabh = get(gca, 'xlabel');
-set(xlabh,'Position', [5 0 0])
+%xlabh = get(gca, 'xlabel');
+%set(xlabh,'Position', [5 0 0])
 
-titleh = get(gca, 'title');
-set(titleh, 'Position', get(titleh,'Position') + [1 0 0])
+%titleh = get(gca, 'title');
+%set(titleh, 'Position', get(titleh,'Position') + [1 0 0])
 
 hold off
-filename = strcat(path, plotTitle, '.png');
-saveas(fig, filename{1}, 'png');
+%filename = strcat(path, plotTitle, '.png');
+%saveas(fig, filename{1}, 'png');
 end
 
--- research-project/evaluation/ReassignmentTest_generatePlots.m
+++ research-project/evaluation/ReassignmentTest_generatePlots.m
@@ -1,8 +1,8 @@
 for pairs = 1:3
 
-    folder = {['/home/kunterbunt/dev/omnetpp-5.0/samples/research-project/research-project/results/ReassignmentTest/' num2str(pairs) 'pairs/']};   
+    folder = {['/home/kunterbunt/dev/omnetpp-5.0/samples/research-project/research-project/results/ReassignmentTest-50RBs/' num2str(pairs) 'pairs/']};   
 
     titles = {['RA-Scheduler--' num2str(pairs) 'pairs--50RBs']};
 
-    RBs_generatePlotForFolder(folder{1}, titles(1));    
+    RBs_generatePlotForFolder(folder{1}, titles(1), [0 1E8]);    
 end
